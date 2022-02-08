83245ba00cfe69d4bdc9571c500d79769ac8617d
Smital Desai <desaismital@gmail.com>
2022-02-08 17:14:48 +0530

    Add place holder and resources for b tree

---
+++ trees/b_tree.py
@@ -0,0 +1,5 @@
+# Inserting a key on a B-tree in Python
+# new nodes have to be inserted only at the leaf node
+# all the leaf nodes should be at the same level
+# https://www.programiz.com/dsa/insertion-into-a-b-tree
+# https://www.geeksforgeeks.org/insert-operation-in-b-tree/
